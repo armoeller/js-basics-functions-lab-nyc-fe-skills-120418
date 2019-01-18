baf3f940878d5a279efa619a43c93dc1e831129a
Andy Moeller <ar.moeller1@gmail.com>
2019-01-18 01:42:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+// const someBlock = 264
+// const startValue = 42
+
+// function distanceFromHqInBlocks(someValue) {
+// if ( startValue > someValue ){
+//   return startValue - someValue
+// } else { return someValue - startValue
+// }
+// }
+
+// const distanceInBlocks = someBlock * someValue 
+
+const startingPoint = 42
+const feetInBlock = 264
+
+function distanceFromHqInBlocks(block) {
+ if(block < startingPoint){
+   return startingPoint - block
+  } else { 
+    return block - startingPoint
+ } 
+}
+
+function distanceFromHqInFeet(block) {
+  if (block < startingPoint){
+    return (startingPoint - block)  * feetInBlock
+  } else {
+    return (block - startingPoint) * feetInBlock
+  }
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+   if (endingBlock < startingBlock) {
+    return (startingBlock - endingBlock) * feetInBlock
+  } else {
+    return (endingBlock - startingBlock) * feetInBlock
+  }
+}
+
+function 
